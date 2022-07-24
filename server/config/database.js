1958cd68ddec31dc45496743a1cd0a27f3a0339f
1223v <78861124+1223v@users.noreply.github.com>
2022-07-25 00:13:17 +0900

    Delete database.js

--- server/config/database.js
+++
@@ -1,8 +0,0 @@
-module.exports= {
-  //host : 'https://korea-app-beqvu.run.goorm.io/',
-  host : 'photoplace.cwepcblcfcpe.ap-northeast-2.rds.amazonaws.com',
-  user : "paul1223",
-  password : "photoplace12!", 
-  database : "photoplace",
-  port: "3306",
-};
